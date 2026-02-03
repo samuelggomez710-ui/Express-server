4a69271bf0fcc8fd574ab00147f7fc371aa72689
Samuel <samuelggomez710@gmail.com>
2026-02-03 12:18:11 -0600

    Fixed code

--- server/app.js
+++ server/app.js
@@ -1,32 +1,23 @@
-    // import files and packages up here
-// The web framework used to build server
+// import files and packages up here
 const express = require('express');
-// Logs every request in the terminal
 const morgan = require('morgan');
-// Loads your top spots data so you can send it to /data
 const data = require('./data.json');
 
-    // create your express server below
 const app = express();
 
-    // add your routes and middleware below
-// Tell app to use morgan to log requests
+// Log requests using morgan's 'dev' format
 app.use(morgan('dev'));
-// Lets server load front end file
-app.use(express.static(`${__dirname}/..`));
-// Serve index.html on root path
-app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/../index.html`);
-});
-//When someone visits the server it will respond with the data from data.json, 200 status code means ok
+
+// Respond with top spots data
 app.get('/data', (req, res) => {
     res.status(200).json(data);
-    res.setHeader('Content-Type', 'text/plain');
-    // Send back the response body and end the response
-    res.end("It's working");
 });
 
-    // finally export the express application
-// Allows test runner and index.js to import Express app
+app.get('/', (req, res) => {
+    res.status(200);
+    res.type('text/html');
+    res.send('Filler text');
+});
+
+// Export the app for tests and for the server starter
 module.exports = app;
-// Test message to see update date in github
